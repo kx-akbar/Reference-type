e7a37205364a98ed32ddc869b1fdcf7808c87d8a
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-03-30 11:17:21 +0500

    sort

---
+++ #4Advanced Array/sort.js
@@ -0,0 +1,20 @@
+// Number sort()
+
+// Agar array ichidagi sonlar 10dan kichik bo'lsa biz shunchaki sort() shunday qilib ishlatsak bo'ladi Lekin agar array ichidagi
+// sonlar 10dan katta bo'lsa bu kod to'g'ri ishlamaydi
+
+// let ar = [4, 1, 3, 2, 5];
+
+// let res = ar.sort();
+
+// console.log(res); // [1, 2, 3, 4, 5]
+
+// ========================================
+
+// Biz agar har doim to'g'ri sort qilmoqchi bo'lsak ushbu kod"dan foydalanamiz
+
+// let ar = [41, 19, 31, 25, 53];
+
+// let res = ar.sort((a, b) => a - b);
+
+// console.log(res); // [ 19, 25, 31, 41, 53 ]
