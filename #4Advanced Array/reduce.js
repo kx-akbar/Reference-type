75c4d3bc3c8dd33787704ece23259d5e630d60bd
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-03-30 11:14:45 +0500

    reduce

---
+++ #4Advanced Array/reduce.js
@@ -0,0 +1,9 @@
+// reduce()
+
+// reduce bizga array ichidagi malumotlarni hisoblash umumiy yig'indini topish uchun juda qo'l keladi.
+
+// let ar = [1, 2, 3, 4, 5];
+
+// let res = ar.reduce((total, value) => total + value);
+
+// console.log(res); // 15
