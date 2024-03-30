b9a7b1809a13f6fdfbf8da89483245b73dc34783
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-03-30 11:12:40 +0500

    map

---
+++ #4Advanced Array/map.js
@@ -0,0 +1,28 @@
+Map;
+
+// map bizga array bo'yicha loop qilib beradi va uni ichida return yozilishi shart
+
+// let ar = [1, 2, 3, 4, 5];
+
+// Example:
+// let res = ar.map((val, index) => {
+//   if (index % 2 === 0) {
+//     return val;
+//   } else return 0;
+// });
+
+// console.log(res); // [ 1, 0, 3, 0, 5 ]
+
+// ====================================================
+
+// Agar map ichida return bo'lmasa automatic undefined bo`lib ketadi
+
+// let ar = [1, 2, 3, 4, 5];
+
+// let res = ar.map((val, index) => {
+//   if (index % 2 === 0) {
+//     return val;
+//   }
+// });
+
+// console.log(res); // [ 1, undefined, 3, undefined, 5 ]
