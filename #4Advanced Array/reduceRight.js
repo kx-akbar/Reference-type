2e88e9f9239af2c53839768dbf2d431c793fe8f2
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-03-30 11:15:57 +0500

    reduceRight

---
+++ #4Advanced Array/reduceRight.js
@@ -0,0 +1,8 @@
+// reduceRight()
+
+// reduceRight bizga o'ng tarafdan boshlab malumotlarni hisoblaydi
+
+// let arr = [20, 45, 30, 100, 240];
+// let res2 = ar.reduceRight((total, value) => total - value);
+
+// console.log(res2); // 13
