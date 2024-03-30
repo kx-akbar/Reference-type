63eef06db36f18bfda1ba4c957e7859d776326a8
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-03-30 11:20:04 +0500

    string sort

--- #4Advanced Array/sort.js
+++ #4Advanced Array/sort.js
@@ -18,3 +18,26 @@
 // let res = ar.sort((a, b) => a - b);
 
 // console.log(res); // [ 19, 25, 31, 41, 53 ]
+
+// ====================================================================
+
+// String sort()
+
+// Agar array elementlari ichida katta harif yo'q bo'lsa va hammasida kichik hariflar bo'lsa bizga bu usul qo'l keladi lekin
+// agar katta harif bor bo'lsa bu kod xato ishlaydi
+
+// let ar = ["apple", "kiwi", "orange", "banana"];
+
+// let res = ar.sort();
+
+// console.log(res); // [ 'apple', 'banana', 'kiwi', 'orange' ]
+
+// ========================================
+
+// Biz har qanday holatda ham to'g'ri sort qilish uchun bu kod"dan foydalanamiz
+
+// let ar = ["apple", "Kiwi", "orange", "Banana"];
+
+// let res = ar.sort((a, b) => a.localeCompare(b));
+
+// console.log(res); // [ 'apple', 'Banana', 'Kiwi', 'orange' ]
