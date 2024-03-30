1135f4f9bc7eb566b7af5451c2170801bca4c7a1
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-03-30 11:12:17 +0500

    filter

---
+++ #4Advanced Array/filter.js
@@ -0,0 +1,9 @@
+// filter()
+
+// filter bizga array ichidagi elementlar"ni filterlash uchun hizmat qiladi va u nima bo'lganda ham arrayni to'liq aylanib chiqadi
+
+// const arr = [1, 4, 6, 23, 20, 34];
+
+// const res = arr.filter((value) => value.id >= 20); // => 23, 20, 34
+
+// console.log(res);
